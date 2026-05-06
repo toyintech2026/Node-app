2c8338428e914dcf927b40f676bac4ad0afb50c3
toyin <adetoyinife@gmail.com>
2026-05-06 22:05:13 +0100

    made new change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 
-  res.send('🚀 DevOps Capstone App is Running!');
+  res.send('🚀 New Toyin DevOps Capstone App is Running!');
 
 });
 
